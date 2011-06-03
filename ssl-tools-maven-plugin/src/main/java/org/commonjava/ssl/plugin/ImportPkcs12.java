3c079b85ae09b2051afabbec119a81a91a052fbb
John Casey <jdcasey@commonjava.org>
2011-06-03 16:56:46 -0400

    updating license to GPLv3, adjusting java version to 1.5, and refactoring some of the package names to simplify things a bit more.

--- ssl-tools-maven-plugin/src/main/java/org/commonjava/ssl/plugin/ImportPkcs12.java
+++ ssl-tools-maven-plugin/src/main/java/org/commonjava/ssl/plugin/ImportPkcs12.java
@@ -1,26 +1,27 @@
 /*
- *  Copyright (C) 2011 John Casey.
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *  
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (c) 2011 Red Hat, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses>.
  */
 
 package org.commonjava.ssl.plugin;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.commonjava.ssl.CertificateImporter;
 import org.commonjava.ssl.SSLToolsException;
-import org.commonjava.ssl.in.CertificateImporter;
 
 import java.io.File;
 
@@ -50,7 +51,6 @@ public class ImportPkcs12
      */
     private String certpass;
 
-    @Override
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
--- ssl-tools-maven-plugin/src/main/java/org/commonjava/ssl/plugin/ImportServerCert.java
+++ ssl-tools-maven-plugin/src/main/java/org/commonjava/ssl/plugin/ImportServerCert.java
@@ -1,18 +1,19 @@
 /*
- *  Copyright (C) 2011 John Casey.
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *  
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (c) 2011 Red Hat, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses>.
  */
 
 package org.commonjava.ssl.plugin;
@@ -25,8 +26,8 @@ import org.apache.maven.model.Site;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.commonjava.ssl.CertificateImporter;
 import org.commonjava.ssl.SSLToolsException;
-import org.commonjava.ssl.in.CertificateImporter;
 import org.sonatype.aether.RepositorySystemSession;
 import org.sonatype.aether.repository.MirrorSelector;
 import org.sonatype.aether.repository.RemoteRepository;
@@ -96,7 +97,6 @@ public class ImportServerCert
     
     private CertificateImporter importer;
 
-    @Override
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
